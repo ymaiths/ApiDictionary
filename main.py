1da011e0799e2b30f7b9136b1fa4c7d05137583a
melchanatan <chanatan@outlook.com>
2025-04-22 23:22:30 +0700

    feat: update search query to Thai slang translation

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ os.environ["TAVILY_API_KEY"] = "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"
 
 def search_flow():
     tools = TavilySearchResults()
-    result = tools.invoke("What is the weather in Tokyo?")
+    result = tools.invoke("คำว่า ฉ่ำ แปลว่าอะไรในภาษาสแลง")
+    print(result)
 
 
 if __name__ == "__main__":
