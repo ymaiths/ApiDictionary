15613cb3d61cd4f883eaa341f1c607a7f654035f
mels <chanatan@outlook.com>
2025-05-15 18:49:14 +0700

    feat: enhance slang retrieval by integrating LLM for meaning lookup and refactor API endpoints

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI,HTTPException
+import sys
+from pymongo.server_api import ServerApi
+import pymongo
+from fastapi import FastAPI, HTTPException
+from main import get_slang_meaning
+
 app = FastAPI()
 
-import pymongo
-from pymongo.server_api import ServerApi
-import sys
 
 uri = "mongodb+srv://admin:1234@thai-slang-dict.10ixf.mongodb.net/?retryWrites=true&w=majority&appName=thai-slang-dict"
 # Create a new client and connect to the server
@@ -12,32 +14,23 @@ try:
 except pymongo.errors.ConfigurationError:
     print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
     sys.exit(1)
-    
-db = client["thai-slang-dict"]
-slang_collection=db["slangs"]
 
-items = []
+db = client["thai-slang-dict"]
+slang_collection = db["slangs"]
+
+
 @app.get("/")
 def root():
-    return {"Hello":"World"}
+    return {"Hello": "World"}
+
 
 @app.get("/find")
-def find_slang(slang: str) -> str: #func_name(param_name: param_data_type ) -> return_data_type
+# func_name(param_name: param_data_type ) -> return_data_type
+def find_slang(slang: str) -> str:
     result = slang_collection.find_one({"key": slang})
     if result is None:
-        # get meaning from LLM
-        raise HTTPException(status_code=404, detail="Slang not found")
-    
-    return result["value"]
-
-@app.post("/save")
-def save_slang(slang,meaning: str):
-    slang_collection.insert_one({"key": slang, "value": meaning})
-    return "saving complete"
-
-@app.delete("/delete")
-def delete_slang(slang: str):
-    result = slang_collection.delete_one({"key": slang})
-    if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Slang not found")
-    return "Slang deleted successfully"
+        llm_result = get_slang_meaning(slang)
+        slang_collection.insert_one({"key": slang, "value": llm_result})
+        return llm_result
+    else:
+        return result["value"]
